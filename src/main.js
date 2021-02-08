261d920c9231ca296885faab6e1ddaad6f803350
fzxiao233 <fzxiao@outlook.com>
2021-02-08 16:33:44 +0800

    feat(main): 切换至 Vue cli
    Signed-off-by: fzxiao233 <fzxiao@outlook.com>

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App'
+
+Vue.config.productionTip = false
+
+App.mpType = 'app'
+
+// Vue.prototype.serverUrl = "http://tencent.terrytengli.com:8002/";
+
+const app = new Vue({
+	...App
+})
+app.$mount()
