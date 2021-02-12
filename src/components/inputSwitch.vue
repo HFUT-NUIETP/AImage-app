5adcc0c0f8687df9732eb3073d9b893becb86de8
fzxiao233 <fzxiao@outlook.com>
2021-02-12 12:01:27 +0800

    comp(inputSwitch): 定义 Switch
    Signed-off-by: fzxiao233 <fzxiao@outlook.com>

---
+++ src/components/inputSwitch.vue
@@ -0,0 +1,14 @@
+<template>
+    <switch :checked="checked" @click="click" style="transform:scale(0.7)" color="#6200ee"/>
+</template>
+
+<script>
+    export default {
+        name: "inputSwitch",
+        props: ["checked", "click"]
+    }
+</script>
+
+<style scoped>
+
+</style>
